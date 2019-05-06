01d29cf09594ddd70062c054bae9ca7129c8d104
Owen Mason <owenmason353@gmail.com>
2019-05-05 22:10:08 -0400

    fixed passcode

--- main.js
+++ main.js
@@ -8,33 +8,33 @@ function checkPasscodeTest() {
 
 function checkPasscode1() {
     console.log(document.getElementById("passcode").value);
-    if (document.getElementById("passcode").value == "test") {
+    if (document.getElementById("passcode").value == "1865") {
         document.getElementById("lockImage").src = "./Images/unlocked.png"
-        document.getElementById("lockImage").stlye.width = "1865";
+        document.getElementById("lockImage").stlye.width = "40%";
     }
 }
 
 function checkPasscode2() {
     console.log(document.getElementById("passcode").value);
-    if (document.getElementById("passcode").value == "test") {
+    if (document.getElementById("passcode").value == "1376") {
         document.getElementById("lockImage").src = "./Images/unlocked.png"
-        document.getElementById("lockImage").stlye.width = "1376";
+        document.getElementById("lockImage").stlye.width = "40%";
     }
 }
 
 function checkPasscode3() {
     console.log(document.getElementById("passcode").value);
-    if (document.getElementById("passcode").value == "test") {
+    if (document.getElementById("passcode").value == "6429") {
         document.getElementById("lockImage").src = "./Images/unlocked.png"
-        document.getElementById("lockImage").stlye.width = "6429";
+        document.getElementById("lockImage").stlye.width = "40%";
     }
 }
 
 function checkPasscode4() {
     console.log(document.getElementById("passcode").value);
-    if (document.getElementById("passcode").value == "test") {
+    if (document.getElementById("passcode").value == "3174") {
         document.getElementById("lockImage").src = "./Images/unlocked.png"
-        document.getElementById("lockImage").stlye.width = "3174";
+        document.getElementById("lockImage").stlye.width = "40%";
     }
 }
 
